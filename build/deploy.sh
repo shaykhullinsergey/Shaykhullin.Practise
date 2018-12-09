05d965091d4f79bf5ca59fcff883d10b16067219
Sergey <shaykhullinsergey@gmail.com>
2018-12-09 14:44:38 +0300

    Reorder

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-cd ..
+
+shell2http -host 92.63.96.49 -port 8080 /deploy "git pull && ./deploy.sh" &> /dev/null &
+echo "shell2http: DONE"
+
+cd ./nginx
+cp default /etc/nginx/sites-available/default
+service nginx restart
+echo "nginx: DONE"
+
+cd ../..
 
 docker-compose down
 docker-compose build
 docker-compose up -d
 
-cd ./build/nginx
-cp default /etc/nginx/sites-available/default
-service nginx restart
-
-cd ..
-
-shell2http -host 92.63.96.49 -port 8080 /deploy "git pull && ./deploy.sh" &> /dev/null &
-
-echo "Deploy: DONE"
+echo "docker-compose: DONE"
