e5f65712b028342ff889582107cb579f3c0e3cb2
Sergey <shaykhullinsergey@gmail.com>
2018-12-09 14:21:11 +0300

    Deploy improvements

--- build/deploy.sh
+++ build/deploy.sh
@@ -7,9 +7,9 @@ docker-compose build
 docker-compose up -d
 
 cd ./build/nginx
-mv default /etc/nginx/sites-avaliable/default
+mv default /etc/nginx/sites-avaliable/
 service nginx restart
 
 cd ..
 
-shell2http -host 92.63.96.49 -port 8080 /deploy "./deploy.sh" &> /dev/null &
+shell2http -host 92.63.96.49 -port 8080 /deploy "./deploy.sh"
