57614883c50b1db0f2ce313331e85033e1a365c0
Sergey <shaykhullinsergey@gmail.com>
2018-12-09 12:52:44 +0300

    Add frontend project

--- src/Practice.Web/Components/PracticeRouterComponent.cs
+++ src/Practice.Web/Components/PracticeRouterComponent.cs
@@ -7,10 +7,6 @@ namespace Practice
 		public PracticeRouterComponent()
 		{
 			Controller<
-				IndexController,
-				IndexRouteComponent>("");
-			
-			Controller<
 				ProfileController,
 				ProfileRouteComponent>("profiles");
 
