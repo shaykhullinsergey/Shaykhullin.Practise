b958edfddb80420350d8174c24265684de2bccef
Ilya Osadchy <ilia.osadchy@bostongene.com>
2018-12-12 19:47:13 +0300

    fix bugs

--- src/Practice.Frontend/src/components/Histogram/index.js
+++ src/Practice.Frontend/src/components/Histogram/index.js
@@ -1,221 +1,166 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import _capitalize from 'lodash/capitalize';
 import * as d3 from 'd3';
-// import './Histogram.css';
+import './Histogram.css';
 
-export default class Histogram extends Component {
-    static rightRoundedRect(x, y, width, height, radius) {
-        return `M${x},${y + height}v${radius
-        - height}a${radius},${radius} 0 0 1 ${radius},${-radius}h${width
-        - 2 * radius}a${radius},${radius} 0 0 1 ${radius},${radius}v${height - radius}h${width}z`;
-    }
+const colorMap = index => ({
+    0: '#84bdff',
+    1: '#2b7cda',
+    2: '#1d5ca4',
+    3: '#f55246',
+    4: '#ba160a',
+    5: '#ba68c8',
+    6: '#a536a8',
+    7: '#810f7c'
+}[index]);
+
+export default class NeoantigensTypesChart extends Component {
+    static propTypes = {
+        chartData: PropTypes.arrayOf(
+            PropTypes.shape({
+                label: PropTypes.string.isRequired,
+                value: PropTypes.number.isRequired
+            })
+        ).isRequired,
+        setTypeColorClass: PropTypes.func.isRequired
+    };
 
     constructor(props) {
         super(props);
-        this.margin = {
-            top: 20,
-            right: 50,
-            bottom: 50,
-            left: 50
+        this.margin = {top: 20, bottom: 0, left: 0, right: 0};
+        this.width = 540;
+        this.height = 44;
+        this.radius = 3;
+        this.state = {
+            hoveredLabel: ''
         };
-        this.xAxisHeight = 2;
-        this.mainColor = '#9fc4ee';
+
         this.update = this.update.bind(this);
     }
 
     componentDidMount() {
-        const {margin} = this;
-        this.width = 800
-        this.height = (this.width * 4) / 7 - this.margin.top - this.margin.bottom;
-        d3.select(this.histogram).style('width', `${this.width + margin.left}px`);
         this.update();
     }
 
-    componentDidUpdate(prevProps) {
-        if (prevProps !== this.props) {
-            this.update();
-        }
-    }
-
     update() {
-        const {
-            margin, width, height, xAxisHeight, mainColor
-        } = this;
-        const {
-            chart, patientNodeColor, closestPatientNode, currentPatientValue
-        } = this.props;
-
-        const updatedChart = {
-            values:
-                Object.keys(chart.values).map(e => ({
-                        value: e,
-                        count: chart.values[e]
-                    })
+        const {width, height, margin, radius} = this;
+        const {chartData, setTypeColorClass} = this.props;
+        const total = d3.sum(chartData, d => d.value);
+        let index = 0;
+        const barData = chartData
+            .map(bar => ({...bar, calcValue: (bar.value / total) * 100}))
+            .map((bar, barIndex, barArray) =>
+                barArray.reduce(
+                    (result, elem, index) => ({
+                        ...result,
+                        barIndex,
+                        [index]: index > barIndex ? 0 : index === barIndex ? bar.value : barArray[index].calcValue
+                    }),
+                    {...bar}
                 )
-        };
-
-        // gridlines in x axis function
-        function makeXGridlines() {
-            return d3
-                .axisBottom(x)
-                .tickFormat((d, i) => (i % 4 === 0 ? d : null))
-                .tickPadding(8);
-        }
-
-        // gridlines in y axis function
-        function makeYGridlines() {
-            return d3
-                .axisLeft(y)
-                .ticks(4)
-                .tickPadding(10);
-        }
-
-        function identationToClosestLeftChains(a) {
-            return a < x.bandwidth() * 5 ? 40 : 0;
-        }
-
-        function identationToClosestRightChains(a) {
-            return width - a < x.bandwidth() * 5 ? 40 : 0;
-        }
-
-        function showTooltip(d) {
-            tooltip
-                .html(
-                    `<span class="histogram__tooltip-amount">${currentPatientValue.toFixed(3)}</span>
-          <span class="histogram__tooltip-row histogram__tooltip-title">Current patient</span>`
-                )
-                .style(
-                    'left',
-                    `${x(d.value)
-                    + margin.left
-                    + identationToClosestLeftChains(x(d.value))
-                    + x.bandwidth() / 2
-                    - identationToClosestRightChains(x(d.value))
-                    + -tooltip.node().getBoundingClientRect().width / 2}px`
-                );
-        }
-
-        // clear before redrawing
-        d3.select(this.histogram)
-            .selectAll('*')
-            .remove();
-        // create chart
-        const svg = d3
-            .select(this.histogram)
-            .attr('width', width + margin.left + margin.right)
-            .attr('height', height + margin.top + margin.bottom)
-            .append('g')
-            .attr('transform', `translate(${margin.left}, ${margin.top})`);
+            );
+        const dataset = d3.stack().keys(chartData.map((clone, index) => index))(barData);
+        const sortedDataset = dataset.map((bar, index) => [bar[index]]);
         const x = d3
-            .scaleBand()
+            .scaleLinear()
             .rangeRound([0, width])
-            .paddingInner(0.2);
-        const y = d3.scaleLinear().range([height, 0]);
-        x.domain(updatedChart.values.map(d => d.value));
-        y.domain([0, d3.max(updatedChart.values, d => d.count)]).nice();
-        const tooltip = d3.select(this.histogramTooltip);
-
-        // add the X gridlines
-        svg
-            .append('g')
-            .attr('class', 'grid')
-            .attr('transform', `translate(0,${height})`)
-            .call(makeXGridlines().tickSize(5));
-
-        // add the Y gridlines
-        svg
-            .append('g')
-            .attr('class', 'grid')
-            .call(makeYGridlines().tickSize(-width));
-
-        svg
-            .append('text')
-            .attr('class', 'axis-title')
-            .attr('x', width / 2)
-            .attr('y', height)
-            .attr('dy', '2.75em')
-            .style('text-anchor', 'middle')
-            .text(updatedChart.xaxisLabel);
-
-        svg
-            .append('text')
-            .attr('class', 'axis-title')
-            .attr('transform', 'rotate(-90)')
-            .attr('y', -margin.left)
-            .attr('x', -height / 2)
-            .attr('dy', '1em')
-            .style('text-anchor', 'middle')
-            .text(updatedChart.yaxisLabel);
-
-        const chainGroup = svg
-            .append('g')
+            .domain([0, 100]);
+        const svg = d3
+            .select(this.stackedChart)
+            .attr('width', width + margin.left + margin.top)
+            .attr('height', height + margin.top + margin.bottom)
+            .append('g');
+        const bar = svg
             .selectAll('g')
-            .data(updatedChart.values)
+            .data(sortedDataset)
+            .enter();
+        bar
+            .selectAll('text')
+            .data(d => d)
             .enter()
+            .filter(d => x(d[1]) - x(d[0]) > 50)
+            .append('text')
+            .attr('class', 'neoantigens-types-chart__chart-text')
+            .attr('transform', d => `translate(${x(d[0])},${margin.top - 10})`)
+            .text(d => `${d.data.value}%`);
+        bar
             .append('g')
-            .attr('class', 'histogram__chain-group');
-
-        chainGroup
+            .attr('transform', `translate(${margin.left},${margin.top})`)
+            .selectAll('path')
+            .data(d => d)
+            .enter()
             .append('path')
-            .attr('class', 'histogram__background-bar')
-            .attr('d', d => Histogram.rightRoundedRect(x(d.value), y(d.count), x.bandwidth(), height - y(d.count), 3))
-            .attr('fill', '#ffffff');
-
-        // append the rectangles for the bar chart
-        chainGroup
-            .append('path')
-            .attr('class', 'histogram__bar')
-            .attr('d', d => Histogram.rightRoundedRect(x(d.value), y(d.count), x.bandwidth(), height - y(d.count), 3))
-            .attr('fill', d => (d.value === closestPatientNode.value ? patientNodeColor : mainColor))
-            .style('opacity', 0.75);
-
-        chainGroup
-            .filter(d => d.value === closestPatientNode.value)
-            .append('line')
-            .attr('stroke', '#1b2834')
-            .attr('stroke-dasharray', 6)
-            .attr('x1', d => x(d.value) + x.bandwidth() / 2)
-            .attr('y1', -20)
-            .attr('x2', d => x(d.value) + x.bandwidth() / 2)
-            .attr('y2', d => y(d.count))
-            .each((d) => {
-                showTooltip(d);
+            .attr('d', d => {
+                if (sortedDataset.length === 1)
+                    return `M${x(d[0])},${height - radius}v${2 * radius -
+                    height}a${radius},${radius} 0 0 1 ${radius},${-radius}h${width -
+                    2 * radius}a${radius},${radius} 0 0 1 ${radius},${radius}v${height -
+                    2 * radius}a${radius},${radius} 0 0 1 ${-radius},${radius}h${-(
+                        width -
+                        2 * radius
+                    )}a${radius},${radius} 0 0 1 ${-radius},${-radius}z`;
+                if (d.data.barIndex === 0)
+                    return `M${x(d[0])},${height - radius}v${2 * radius -
+                    height}a${radius},${radius} 0 0 1 ${radius},${-radius}h${x(d[1]) - x(d[0]) - radius}v${height}h${-(
+                        x(d[1]) -
+                        x(d[0]) -
+                        radius
+                    )}a${radius},${radius} 0 0 1 ${-radius},${-radius}z`;
+                if (d.data.barIndex === sortedDataset.length - 1) {
+                    return `M${x(d[0])},${height}v${-height}h${x(d[1]) -
+                    x(d[0]) -
+                    radius}a${radius},${radius} 0 0 1 ${radius},${radius}v${height -
+                    2 * radius}a${radius},${radius} 0 0 1 ${-radius},${radius}h${-(x(d[1]) - x(d[0]) - radius)}z`;
+                }
+                return `M${x(d[0])},${height}v${-height}h${x(d[1]) - x(d[0])}v${height}h${-(x(d[1]) - x(d[0]))}z`;
+            })
+            .attr('fill', (d) => colorMap(d.data.index))
+            .style('opacity', 0.75)
+            .on('mouseover', d => {
+                bar
+                    .selectAll('path')
+                    .filter(elem => elem.data.barIndex === d.data.barIndex)
+                    .style('opacity', 1);
+                this.setState({hoveredLabel: d.data.label});
+            })
+            .on('mouseout', () => {
+                bar.selectAll('path').style('opacity', 0.75);
+                this.setState({hoveredLabel: ''});
             });
-
-        // add the X Axis
-        svg
-            .append('rect')
-            .attr('transform', `translate(0,${height})`)
-            .attr('width', width)
-            .attr('height', xAxisHeight)
-            .attr('fill', '#9bbce6');
     }
 
     render() {
-        const {chartTitle} = this.props;
+        const {chartData} = this.props;
+        const {hoveredLabel} = this.state;
         return (
-            <div className="histogram">
-                <div className="histogram__title-container" ref={node => (this.titleContainer = node)}>
-                    <div className="histogram__title">{chartTitle}</div>
+            <div className="neoantigens-types-chart__chart-wrapper">
+                <svg xmlns="http://www.w3.org/2000/svg" ref={node => (this.stackedChart = node)}/>
+                <div className="neoantigens-types-chart__legend-list">
+                    {chartData.map((neoantigen, index) => {
+                        const textClasses = classNames({
+                            'neoantigens-types-chart__legend-item-text': true,
+                            'neoantigens-types-chart__legend-item-text--selected': neoantigen.label === hoveredLabel
+                        });
+                        return (
+                            <div key={neoantigen.label} className="neoantigens-types-chart__legend-item">
+                                <div
+                                    className="neoantigens-types-chart__legend-item-indicator"
+                                    style={{background: colorMap(neoantigen.index)}}
+                                />
+                                <div className={textClasses}>
+                                    <div
+                                        className="neoantigens-types-chart__legend-item-label">{_capitalize(neoantigen.label)}</div>
+                                    <div
+                                        className="neoantigens-types-chart__legend-item-percent">{Math.round(neoantigen.value)}%
+                                    </div>
+                                </div>
+                            </div>
+                        );
+                    })}
                 </div>
-                <svg xmlns="http://www.w3.org/2000/svg" ref={node => (this.histogram = node)}/>
-                <div className="histogram__tooltip" ref={node => (this.histogramTooltip = node)}/>
             </div>
         );
     }
 }
-
-Histogram.propTypes = {
-    chart: PropTypes.shape({
-        values: PropTypes.array.isRequired,
-        xaxisLabel: PropTypes.string.isRequired,
-        yaxisLabel: PropTypes.string.isRequired
-    }).isRequired,
-    closestPatientNode: PropTypes.shape({
-        count: PropTypes.number,
-        value: PropTypes.number
-    }).isRequired,
-    patientNodeColor: PropTypes.string.isRequired,
-    currentPatientValue: PropTypes.number.isRequired,
-    chartTitle: PropTypes.string.isRequired
-};
--- src/Practice.Frontend/src/pages/answer/index.js
+++ src/Practice.Frontend/src/pages/answer/index.js
@@ -1,22 +1,49 @@
 import React from "react";
 import {Component} from "react";
 import {connect} from "react-redux";
-import PieChart from "../../components/pieChart";
-import Histogram from "../../components/Histogram";
+import _toLower from 'lodash/toLower';
+import AnswerPieChart from "../../components/answerPieChart";
+import NeoantigensTypesChart from "../../components/Histogram";
 
 class Answer extends Component {
     render() {
         return <div className="column is-fullwidth">
-            <PieChart size={[5000, 5000]} innerRadius={250}
-                      result={{results: this.props.answer.results.filter(value => value === true).length}}/>
+            <div className="columns is-12">
+                <div className="column is-4" style={{marginTop: '30px', marginLeft: '170px'}}>
+                    <AnswerPieChart size={[5000, 5000]} innerRadius={250}
+                                    result={{results: this.props.answer.results.filter(value => value === true).length}}/>
+                </div>
+                <div className="column is-4" style={{marginTop: '100px'}}>
+                    {this.props.answer.results.map((result, index) =>
+                        result
+                            ? <h1 className="title">{index + 1}: Правильно</h1>
+                            : <h1 className="title">{index + 1}: Неправильно</h1>
+                    )}
+                </div>
+            </div>
+
             <div className="biomarkers-details-page__section">
-                <Histogram chart={{
-                    values: this.props.answer.statistics
-                }}
-                           closestPatientNode={1}
-                           patientNodeColor={1}
-                           currentPatientValue={1}
-                           chartTitle={'Students results'}
+                <NeoantigensTypesChart chartData={
+                    Object.keys(this.props.answer.statistics).map((e, index) => {
+                        const fullValue = 100 / Object.keys(this.props.answer.statistics)
+                            .reduce((number, g) => number + this.props.answer.statistics[g], 0);
+                        console.log(fullValue)
+                        return ({
+                            index,
+                            label: `${e} правильных ответов`,
+                            value: this.props.answer.statistics[e] * fullValue
+                        })
+                    })
+                }
+                                       some={(type) => {
+                                           const colors = {
+                                               frameshift: '#2b7cda',
+                                               'snv at hla anchors, strong binder': '#962294',
+                                               'snv at other sites, strong binder': '#c8b58b',
+                                               'other epitopes': '#f4ae63'
+                                           };
+                                           return colors[_toLower(type)];
+                                       }}
                 />
             </div>
         </div>
